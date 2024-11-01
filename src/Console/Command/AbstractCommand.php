e08af46a1335a42f12cf67702bf1ddfc9b03ae70
Stephan <sb@relaxt.at>
2024-11-01 10:23:27 +0100

    Fix for compatibility of AbstractCommand with new Symfony method signature.

--- src/Console/Command/AbstractCommand.php
+++ src/Console/Command/AbstractCommand.php
@@ -29,7 +29,7 @@ abstract class AbstractCommand extends Command
      *
      * @throws InvalidArgumentException
      */
-    protected function execute(InputInterface $input, OutputInterface $output)
+    protected function execute(InputInterface $input, OutputInterface $output) : int
     {
         $config    = $this->createConfig($input);
         $processor = $this->createProcessor($input, $output);
